742d3c13c50218749c8b1a1b9cf021f9e088fe9e
lix <lix@amtxts.com>
2023-01-10 17:14:14 +0800

    修复二维码识别异常报错

--- app/qrcode/infer_qr.py
+++ app/qrcode/infer_qr.py
@@ -435,8 +435,11 @@ def wechat_decode(img):
         '/home/cpard/attn_ocr/app/qrcode/wechat/detect.caffemodel',
         '/home/cpard/attn_ocr/app/qrcode/wechat/sr.prototxt',
         '/home/cpard/attn_ocr/app/qrcode/wechat/sr.caffemodel')
-    res, points = detect_obj.detectAndDecode(img)
-    return res, points
+    try:
+        res, points = detect_obj.detectAndDecode(img)
+        return res, points
+    except Exception as e:
+        return [''], ['']
 
 
 def resizetodecode(img, cmd):
